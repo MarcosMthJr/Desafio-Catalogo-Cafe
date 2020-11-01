07c1e5fff1a44a521ebabb8dca6d1f4a9757120a
fernandomaia97 <fernandomarinhom.maia@hotmail.com>
2020-11-01 00:01:55 -0300

    Otimizando listagem

--- controller/listar.js
+++ controller/listar.js
@@ -2,7 +2,7 @@
 // inicia a ação assim que o site carrega
 $(document).ready(function(){
 
-// diretório do json	
+// diretório do json
 	var itens = "", url = "json/dados.php";
 	$.ajax({
 		url: url,
@@ -16,38 +16,33 @@ $(document).ready(function(){
 	// função que acontece antes da tabela ser carregada .. se caso o carregamento for lento .. ele mostrara a mensagem "carregando" para o usuario
 	function carregando(){
 			$("h3").html("Carregando...");
-		
+
 	}
 
-	// se acontecer algum erro .. ele retorna essa mensagem	
+	// se acontecer algum erro .. ele retorna essa mensagem
 	function erro(){
 			$("h3").html("Há algum erro");
-		
+
 	}
 
-	// essa função lista todos os dados do json .. direto na tabela 
+	// essa função lista todos os dados do json .. direto na tabela
 	function listar(dados){
-		
-
-		if(dados[0].erro){
+		if (dados[0].erro) {
 			$("h3").html(dados[0].erro);
-		}else{
-
-		// percorrendo o json  e montando o corpo da tabela	
-			for(var i = 0; i < dados.length; i++){
-				
-				itens += "<tr>";
-				itens += "<td>" + dados[i].id + "</td>";
-				itens += "<td>" + dados[i].nome + "</td></b>";
-				itens += "<td>" + dados[i].descricao + "</td>";
-				itens += "</tr>";
+		} else {
+		    // percorrendo o json  e montando o corpo da tabela
+            let tamanho = dados.length;
+			for(var i = 0; i < tamanho; i++){
+				itens += `<tr>
+    				<td>${dados[i].id}</td>
+    				<td>${dados[i].nome}</td>
+    				<td>${dados[i].descricao}</td>
+				</tr>`;
 			}
-			// mandando a variavel itens que tem os dados da tabela e inserindo no corpo da tabela 	
+			// mandando a variavel itens que tem os dados da tabela e inserindo no corpo da tabela
 			$("#tabelaCatalogo tbody").html(itens);
-
 			// limpando o h3 que tinha a mensagem carregando
-			$("h3").html("");	
+			$("h3").html("");
 		}
 	}
 })
-
